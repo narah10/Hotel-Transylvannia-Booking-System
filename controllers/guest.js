d21f6a4d223000c844996d3e2b8f9082aa36d57d
Terrystan Sustal <terrysustal@gmail.com>
2023-11-15 17:05:04 -0800

    Added a GET SINGLE for guest controller/routes

--- controllers/guest.js
+++ controllers/guest.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const mongodb = require("../db/connect");
+const { ObjectId } = require("mongodb");
 // const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res, next) => {
@@ -14,4 +15,23 @@ const getAll = async (req, res, next) => {
     });
 };
 
-module.exports = { getAll };
+const getSingle = async (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json("Must use a valid guest ID to find a guest.");
+        return; // Exit early if the ID is not valid
+    }
+
+    const userId = new ObjectId(req.params.id);
+    const collection = mongodb.getDb().db("hotel-transylvania").collection("guest");
+
+    const result = await collection.findOne({ _id: userId });
+
+    if (result) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(result);
+    } else {
+        res.status(404).json("Guest not found")
+    }
+};
+
+module.exports = { getAll, getSingle };
--- routes/guest.js
+++ routes/guest.js
@@ -5,5 +5,6 @@ const guestsController = require("../controllers/guest");
 
 // This route will get all the students
 router.get("/", guestsController.getAll);
+router.get("/:id", guestsController.getSingle);
 
 module.exports = router;
