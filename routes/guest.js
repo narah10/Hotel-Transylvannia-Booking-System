891f5b1506679dfb6899ba50ff4e8dba1a59ee75
Terrystan Sustal <terrysustal@gmail.com>
2023-11-20 20:15:48 -0800

    Added validation.saveGuest

--- routes/guest.js
+++ routes/guest.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const guestsController = require("../controllers/guest");
+const validation = require('../middleware/validate');
 
 // This route will get all the students
 router.get("/", guestsController.getAll);
 router.get("/:id", guestsController.getSingle);
-router.post("/", guestsController.createGuest);
-router.put('/:id', guestsController.updateGuest);
+router.post("/", validation.saveGuest, guestsController.createGuest);
+router.put('/:id', validation.saveGuest, guestsController.updateGuest);
 router.delete('/:id', guestsController.deleteGuest);
 
 module.exports = router;
