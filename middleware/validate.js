bbbe90179d8c94192a6c992ad41f1ce7d2e2cd44
Terrystan Sustal <terrysustal@gmail.com>
2023-11-20 20:07:52 -0800

    Added saveGuest

---
+++ middleware/validate.js
@@ -0,0 +1,30 @@
+const validator = require('../helpers/validate');
+
+const saveGuest = (req, res, next) => {
+    const validationRule = {
+      guestID: "required|string",  
+      firstName: "required|string",
+      lastName: "required|string",
+      email: "required|string",
+      phone: "required|string",
+      birthDate: "required|string",
+      address: "required|string",
+      dateOfBirth: "required|string",
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        console.log(req.body);
+        if (!status) {
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
+};
+
+module.exports = {
+    saveGuest
+};
